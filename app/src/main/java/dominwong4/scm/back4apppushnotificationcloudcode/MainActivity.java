06afb2a7c454c80080ac4eb89370f3f7f1f5025b
dominwong4 <wonghoyin@Wongho.local>
2016-09-15 17:30:47 -0700

    1st Commit

---
+++ app/src/main/java/dominwong4/scm/back4apppushnotificationcloudcode/MainActivity.java
@@ -0,0 +1,14 @@
+package dominwong4.scm.back4apppushnotificationcloudcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.parse.Parse;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/dominwong4/scm/back4apppushnotificationcloudcode/MainApplication.java
@@ -0,0 +1,42 @@
+package dominwong4.scm.back4apppushnotificationcloudcode;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
+/**
+ * Created by wonghoyin on 15/9/2016.
+ */
+
+public class MainApplication extends Application {
+    private static MainApplication instance = new MainApplication();
+    public static final String APPLICATION_ID = "msQU7s4NATNB8Hkx2l2mVJlui1GbXQzt7Mp5ZIcP";
+    public static final String CLIENT_KEY = "0qEjd7ei4To77jZxJG6QJ8lBipPBYqZp9bsHodZg";
+    public static final String BACK4PAPP_API = "https://parseapi.back4app.com/";
+
+    public MainApplication(){
+        instance = this;
+    }
+
+    public static Context getContext(){
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(APPLICATION_ID)
+                .clientKey(CLIENT_KEY)
+                .server(BACK4PAPP_API).build());
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_VERBOSE);
+        Log.d("Parse","Initialized");
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+        installation.put("GCMSenderId", "966437188652");
+        installation.saveInBackground();
+    }
+}
